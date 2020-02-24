dd96f3f4d5bb6117442023c00f8247253fa408e7
Suresh Amrutham <sureshamrutham91@gmail.com>
2020-02-24 17:11:56 +0530

    pushing code to develop branch

---
+++ src/test/java/com/springawsjava/demo/SpringawsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springawsjava.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringawsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
